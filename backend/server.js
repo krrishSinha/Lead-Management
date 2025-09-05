a92107642a2d8baf6d3212b78fb9e52215141015
Krrish Sinha <sinhakrrish22@gmail.com>
2025-09-05 23:19:38 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,13 @@ const connectDB = require('./db')
 const leadRoutes = require('./routes/lead.routes')
 
 const app = express()
-app.use(cors({ origin: true }))
 app.use(express.json())
 require('dotenv').config()
+
+app.use(cors({ 
+    origin: 'https://lead-management-1-15lg.onrender.com',
+    credentials: true
+ }))
 
 const PORT = process.env.PORT || 8000
 
